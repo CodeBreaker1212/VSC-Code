eec4b8889a80f07814b54923f88eaa9015970482
Cody Owens <codered062403@gmail.com>
2024-02-08 10:38:27 -0500

    2/8/24(2)

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "hello world";
+
+            Console.WriteLine(message);
+        }
+    }
+}
